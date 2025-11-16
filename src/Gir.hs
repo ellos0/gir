bfd9e6b1e9ee1d01f993141596f4d7f9383f7021
ellos0 <mcgibbonellott@gmail.com>
2025-11-15 21:40:08 -0800

    removed old Lexer dependency

--- src/Gir.hs
+++ src/Gir.hs
@@ -1,27 +1,19 @@
 module Gir where
 
-
+import Structures (Statement (Add, Subtract, Multiply, Divide, Power, Assignment, Defun, Application))
 
 addSemicolon :: String -> String
 addSemicolon x = x ++ ";"
 
-convertErrorStatement :: Either CompileError Statement -> Maybe Statement
-convertErrorStatement Right x = Just x
-convertErrorStatement _ = Nothing
-
-codegenFunction :: Expr -> String
-codegenFunction _ = "" --temporary! remove soon!
-
-codegenAtomOrStatement :: Expr -> String
-codegenAtomOrStatement (Atom x) = x
-codegenAtomOrStatement x = codegenStatement x
+codegenFunction :: Statement -> Statement -> String
+codegenFunction _ _ = "" --temporary! remove soon!
 
 codegenStatement :: Statement -> String
-codegenStatement (Add x1 x2) = (codegenAtomOrStatement x1) ++ " + " ++ (codegenAtomOrStatement x2)
-codegenStatement (Subtract x1 x2) = (codegenAtomOrStatement x1) ++ " - " ++ (codegenAtomOrStatement x2)
-codegenStatement (Multiply x1 x2) = (codegenAtomOrStatement x1) ++ " * " ++ (codegenAtomOrStatement x2)
-codegenStatement (Divide x1 x2) = (codegenAtomOrStatement x1) ++ " / " ++ (codegenAtomOrStatement x2)
-codegenStatement (Power x1 x2) = (codegenAtomOrStatement x1) ++ " ^ " ++ (codegenAtomOrStatement x2)
-codegenStatement (Assignment x1 x2) = "auto " ++ x1 ++ " = " ++ (codegenAtomOrStatement x2)
---codegenStatement (Defun x1 x2) = "auto " ++ x1 ++ " = " ++ (codegenAtomOrStatement x2)
-codegenStatement (Application x1) = codegenAtomOrStatement x1 ++ "()"
+codegenStatement (Add x1 x2) = (codegenStatement x1) ++ " + " ++ (codegenStatement x2)
+codegenStatement (Subtract x1 x2) = (codegenStatement x1) ++ " - " ++ (codegenStatement x2)
+codegenStatement (Multiply x1 x2) = (codegenStatement x1) ++ " * " ++ (codegenStatement x2)
+codegenStatement (Divide x1 x2) = (codegenStatement x1) ++ " / " ++ (codegenStatement x2)
+codegenStatement (Power x1 x2) = (codegenStatement x1) ++ " ^ " ++ (codegenStatement x2)
+codegenStatement (Assignment x1 x2) = "auto " ++ x1 ++ " = " ++ (codegenStatement x2)
+--codegenStatement (Defun x1 x2) = (codegenFunction x1 x2)
+codegenStatement (Application x1) = codegenStatement x1 ++ "()"
